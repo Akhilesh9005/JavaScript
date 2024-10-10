2e9346381ba89f168b50ae45f5c1c60315d43235
Akhilesh <akhiles9005@gmail.com>
2024-10-10 07:08:18 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3) //we are using nodejs ,not browser 
+//and code should be readability is high//
+
+console.log("Akhilesh")
+
+
+let name = "Akhilesh"
+let age = 23
+let isLoggedIn = false
+let state = null
+
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/flase
+// null = standlone value
+// underfined => abi rska value defind nhi hua hai 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof "age");
+console.log(typeof undefined);// undefined 
+console.log(typeof null);// object

d7df354693a4d4a1e6f53c5f74821436bdc65bb5
Akhilesh <akhiles9005@gmail.com>
2024-10-10 14:45:50 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+//  problem tb aati jab ham same data type ko comapre nahi karte hai matlab data types ek dusre se alag ho tb 
+
+// 1- comparision karne ke liye data type same ho 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+//===
+
+console.log("2" === 2);

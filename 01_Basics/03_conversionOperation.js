4fe65c16ed64dbf8c9c099e990fe6a846889b494
Akhilesh <akhiles9005@gmail.com>
2024-10-10 08:17:45 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true
+
+// const {score}=req.body
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abd" => NaN
+//true => 1; false=> 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false;
+//"" => false;
+//"Akhilesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);

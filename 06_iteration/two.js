3146baaaadc68b3e97d2bcf82f6c8b27b99aaa90
Akhilesh <akhiles9005@gmail.com>
2024-10-15 15:43:25 +0000

    while and do loop

---
+++ 06_iteration/two.js
@@ -0,0 +1,34 @@
+// syntax of while
+// while (condition) {
+    
+// }
+
+// 1:- while loop
+
+let index = 0
+// while (index <= 10) {
+//    console.log(`value of index is ${index}`);
+//     index = index +2
+// }
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0 
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// 2:- Do loop 
+// syntax of Do loop
+// do {
+    
+// } while (condition);
+// rsme kya hai kaam pahle hota hai condition baad me check hota hai 
+
+let score = 11 
+
+do {
+   console.log(`score is ${score}`);
+    score++
+} while (score <= 10);

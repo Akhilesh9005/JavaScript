c3ae9b9fa6f90cab138f9491fd34a15eed8478db
Akhilesh <akhiles9005@gmail.com>
2024-10-15 19:10:54 +0000

    for each loop

---
+++ 06_iteration/five.js
@@ -0,0 +1,18 @@
+const coding = ["js", "ryby", "java", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+    
+// })
+
+// coding.forEach(  (item) => {
+//     console.log(item);
+    
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+

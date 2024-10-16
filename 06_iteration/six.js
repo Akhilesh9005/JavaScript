fb480880e1a4fc803cda0323ec119abf00c071c0
Akhilesh <akhiles9005@gmail.com>
2024-10-16 08:57:51 +0000

    filter use

---
+++ 06_iteration/six.js
@@ -0,0 +1,35 @@
+// const coding = ["js", "ryby", "java", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+// filter operatins: ye value retun karta  hai lekin esme condition bhi deni padti hai  
+// aur esme agr hum scope {} open karte hai to return likhna padega 
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => num > 4)
+// console.log(newNums);
+// scope ka use 
+// const newNums = myNums.filter( (num) => {
+//      return num > 4
+// } )
+
+// const newNums = []
+// myNums.forEach ( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+
+ 
+
+
+
